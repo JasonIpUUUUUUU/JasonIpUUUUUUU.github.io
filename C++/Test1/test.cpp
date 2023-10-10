ecc23c52429e622a30901c85747647b4cfa61ece
JasonIpUUUUUUU <144679535+JasonIpUUUUUUU@users.noreply.github.com>
2023-10-10 11:44:06 +0100

    Update test.cpp

--- C++/Test1/test.cpp
+++ C++/Test1/test.cpp
@@ -1,11 +1,50 @@
-#include<vector> 
-#include <algorithm>
 #include <string>
 #include <iostream>
+#include <bitset>
+#include <vector>
 
 using namespace std;
 
-int main() {
-    cout << "hello world" << endl;
+char alphabet[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+
+vector<string> dancingBacon(string cText){
+    int index = 0;
+    char *ptr = alphabet;
+    string element = "";
+    vector<string> outputs, binaryCodes;
+    for(char c : cText){
+        if(c != ' '){
+        int character = int(toupper(c)) - 65;
+        bitset<6> binaryRepresentation(character);
+        binaryCodes.push_back(binaryRepresentation.to_string());
+        }
+    }
+    for(string binary: binaryCodes){
+            for(char bit: binary){
+                if(bit == '0'){
+                    index--;
+                }
+                else{
+                    index++;
+                }
+                if(index < 0){
+                    index = 25;
+                }
+                else if(index > 25){
+                    index = 0;
+                }
+                element += alphabet[index];
+            }
+            outputs.push_back(element);
+            element = "";
+    }
+    return outputs;
+}
+
+int main(){
+    vector<string> thing = dancingBacon("This is a test");
+    for(string element : thing){
+        cout << element;
+    }
     return 0;
 }
